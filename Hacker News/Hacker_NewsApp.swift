8559935bf9b2e1be1d072468ca6d1a84dfa00e65
MariaYelfimova <maria.yelfimova@gmail.com>
2020-10-04 08:25:50 +0300

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Maria Yelfimova on 04/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
